0e2a55fc47a3763dec4ac5c262e93fa8613d6a04
Mike Williams <mwilliams@mozilla.com>
2023-01-20 12:54:37 -0500

    WIP: fix #5: add argo support for execution

--- auto_sizing/cli.py
+++ auto_sizing/cli.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
+from jetstream.argo import submit_workflow
 from mozanalysis.utils import hash_ish
 from .size_calculation import SizeCalculation
 from .logging import LogConfiguration
@@ -38,6 +39,59 @@ class ExecutorStrategy(Protocol):
         configuration_map: Optional[Mapping[str, TextIO]] = None,
     ) -> bool:
         ...
+
+@attr.s(auto_attribs=True)
+class ArgoExecutorStrategy:
+    project_id: str
+    dataset_id: str
+    bucket: str
+    zone: str
+    cluster_id: str
+    monitor_status: bool
+    cluster_ip: Optional[str] = None
+    cluster_cert: Optional[str] = None
+    experiment_getter: Callable[[], SizingCollection] = SizingCollection.from_repo
+
+    WORKFLOW_DIR = Path(__file__).parent / "workflows"
+    RUN_WORKFLOW = WORKFLOW_DIR / "run.yaml"
+
+    def execute(
+        self,
+        worklist: Iterable[SizingConfiguration],
+        configuration_map: Optional[Mapping[str, TextIO]] = None,
+    ):
+        if configuration_map is not None:
+            raise Exception("Custom configurations are not supported when running with Argo")
+
+        # experiments_config: Dict[str, List[str]] = {}
+        # for config in worklist:
+        #     experiments_config.setdefault(config.experiment.normandy_slug, []).append(
+        #         # date.strftime("%Y-%m-%d")
+        #     )
+
+        # experiments_config_list = [
+        #     {"slug": slug, "dates": dates} for slug, dates in experiments_config.items()
+        # ]
+
+        targets_config_list = [
+            target_slug for target_slug in worklist
+        ]
+
+        return submit_workflow(
+            project_id=self.project_id,
+            zone=self.zone,
+            cluster_id=self.cluster_id,
+            workflow_file=self.RUN_WORKFLOW,
+            parameters={
+                "targets": targets_config_list,
+                "project_id": self.project_id,
+                "dataset_id": self.dataset_id,
+                "bucket": self.bucket,
+            },
+            monitor_status=self.monitor_status,
+            cluster_ip=self.cluster_ip,
+            cluster_cert=self.cluster_cert,
+        )
 
 
 @attr.s(auto_attribs=True)
@@ -228,6 +282,36 @@ app_id_option = click.option("--app_id", "--app_name", help="Firefox app name")
 run_presets_option = click.option("--run_presets", hidden=True, is_flag=True, default=False)
 
 
+zone_option = click.option(
+    "--zone", default="us-central1-a", help="Kubernetes cluster zone", required=True
+)
+cluster_id_option = click.option(
+    "--cluster_id",
+    "--cluster-id",
+    default="jetstream",
+    help="Kubernetes cluster name",
+    required=True,
+)
+
+monitor_status_option = click.option(
+    "--monitor_status",
+    "--monitor-status",
+    default=True,
+    help="Monitor the status of the Argo workflow",
+)
+
+cluster_ip_option = click.option(
+    "--cluster_ip",
+    "--cluster-ip",
+    help="Kubernetes cluster IP address",
+)
+
+cluster_cert_option = click.option(
+    "--cluster_cert",
+    "--cluster-cert",
+    help="Kubernetes cluster certificate used for authenticating to the cluster",
+)
+
 @cli.command()
 @sizing_name_option
 @project_id_option
@@ -260,3 +344,44 @@ def run(
     )
 
     sys.exit(0 if success else 1)
+
+
+@cli.command()
+@project_id_option
+@dataset_id_option
+@sizing_name_option
+@bucket_option
+@zone_option
+@cluster_id_option
+@monitor_status_option
+@cluster_ip_option
+@cluster_cert_option
+def run_argo(
+    project_id,
+    dataset_id,
+    target_slug,
+    bucket,
+    zone,
+    cluster_id,
+    monitor_status,
+    cluster_ip,
+    cluster_cert,
+):
+    """Runs analysis for the provided date using Argo."""
+    strategy = ArgoExecutorStrategy(
+        project_id=project_id,
+        dataset_id=dataset_id,
+        bucket=bucket,
+        zone=zone,
+        cluster_id=cluster_id,
+        monitor_status=monitor_status,
+        cluster_ip=cluster_ip,
+        cluster_cert=cluster_cert,
+    )
+
+    AnalysisExecutor(
+        project_id=project_id,
+        dataset_id=dataset_id,
+        bucket=bucket,
+        target_slugs=[target_slug] if target_slug else All,
+    ).execute(strategy=strategy)
